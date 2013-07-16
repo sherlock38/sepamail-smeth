bc988a992df15cc7dd78c9d95297cd881c1659f1
bishan.madhoo@gmail.com <bishan.madhoo@gmail.com@c16d36b2-cb39-f675-b753-0845ad468b07>
2013-07-16 10:59:00 +0000

    SMETH 1.3 Developer documentation

---
+++ trunk/doc/developer/api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "SMETHAcknowledgementController",
+        "SMETHAcknowledgementPreferencesController",
+        "SMETHAddressBookController",
+        "SMETHAdvancedPreferencesController",
+        "SMETHAttachmentController",
+        "SMETHAutomaticAcknowledgementController",
+        "SMETHBankPreferencesController",
+        "SMETHComposeMessageController",
+        "SMETHControlFactory",
+        "SMETHExportController",
+        "SMETHHeaderController",
+        "SMETHMail",
+        "SMETHMailPreferencesController",
+        "SMETHMessageHandler",
+        "SMETHMessageListController",
+        "SMETHOptionController",
+        "SMETHRawContentController",
+        "SMETHReadMessageController",
+        "SMETHReportController",
+        "SMETHRequestController",
+        "SMETHUtils"
+    ],
+    "modules": [
+        "smeth"
+    ],
+    "allModules": [
+        {
+            "displayName": "smeth",
+            "name": "smeth"
+        }
+    ]
+} };
+});
